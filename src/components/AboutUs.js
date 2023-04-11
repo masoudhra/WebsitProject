59830110eb8f7fc74315ab05ebc1d94758f03729
Masoud Ramezani <masoudhra@gmail.com>
2023-04-11 15:10:06 +1200

    use of useNavigate for push and replace

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-class AboutUs extends Component {
-    render() {
-        return (
-            <div>
-                <h1>About Us</h1>
-            </div>
-        );
+const AboutUs = () => {
+    const navigate = useNavigate();
+    
+
+
+    //if we don't write replace, we can go back to last page.
+    const clickHandler = () =>{
+        navigate ('/' , {replace: true});
     }
-}
+
+
+    return (
+        <div>
+            <h1>About Us</h1>
+            <br/>
+            <br/>
+            <button onClick={clickHandler}>Go Home</button>
+        </div>
+    );
+};
 
 export default AboutUs;
